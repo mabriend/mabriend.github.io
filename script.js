9173d2843ab26c1ec3259c6cc1ee138d322bdcab
Mathis BRIEND <mathis.briend22@gmail.com>
2024-03-14 16:01:03 +0100

    refacto website

---
+++ script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenuButton = document.getElementById('mobile-menu');
+    const navbar = document.querySelector('.navbar');
+
+    mobileMenuButton.addEventListener('click', function () {
+        navbar.classList.toggle('show');
+    });
+});
+
+
+window.addEventListener('load', function () {
+    document.getElementById('accueil').style.transform = "scale(1.1)";
+})
+
+var aboutMeHeaderElement = document.querySelector('.about-me');
+var observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+            var aboutMeHeaderTitle = document.getElementById('about-me-header-title');
+            aboutMeHeaderTitle.classList.toggle('animateLeft');
+            var aboutMeHeaderDescription = document.getElementById('about-me-header-description');
+            aboutMeHeaderDescription.classList.toggle('animateRight');
+            var header = document.querySelector('header');
+            header.style.visibility = 'visible';
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.5 })
+observer.observe(aboutMeHeaderElement);
+
+var aboutMeIutElement = document.querySelector('.about-me-iut');
+var observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+            var aboutMeHeaderTitle = document.getElementById('about-me-informations-iut');
+            aboutMeHeaderTitle.classList.toggle('animateLeft');
+            var aboutMeHeaderDescription = document.getElementById('about-me-picture-iut');
+            aboutMeHeaderDescription.classList.toggle('animateRight');
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.5 })
+observer.observe(aboutMeIutElement);
